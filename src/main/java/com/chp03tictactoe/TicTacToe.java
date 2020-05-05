e8d6187ee3ff27e15ebe55caf8bef4871597e038
ematulich <emanuel.matulich@mercadolibre.com>
2020-05-05 00:03:18 -0300

    First tdd test and implementation code

--- src/main/java/com/chp03tictactoe/TicTacToe.java
+++ src/main/java/com/chp03tictactoe/TicTacToe.java
@@ -1,4 +1,12 @@
 package com.chp03tictactoe;
 
 public class TicTacToe {
+
+
+    public void placePiece(String piece, int xAxis, int yAxis) throws RuntimeException{
+        if(xAxis < 0 || xAxis > 3){
+            throw new RuntimeException("X-Axis out of range, should be between 1-3");
+        }
+    }
+
 }
--- src/test/java/com/chp03tictactoe/TicTacToeSpec.java
+++ src/test/java/com/chp03tictactoe/TicTacToeSpec.java
@@ -1,4 +1,32 @@
 package com.chp03tictactoe;
 
+import org.junit.BeforeClass;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+
 public class TicTacToeSpec {
+
+    static TicTacToe game;
+
+    @Rule
+    public ExpectedException runtime = ExpectedException.none();
+
+    @BeforeClass
+    public static void setup(){
+        game = new TicTacToe();
+    }
+
+    @Test
+    public void xAxisTooLargeShouldThrowRuntimeException(){
+
+        int x = 4;
+        int y = 2;
+        String piece = "X";
+
+        runtime.expect(RuntimeException.class);
+
+        game.placePiece(piece, x, y);
+
+    }
 }
