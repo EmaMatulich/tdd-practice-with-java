5ea931ef34bd80605919531480c635d1a7c828c7
ematulich <emanuel.matulich@mercadolibre.com>
2020-05-05 11:02:52 -0300

    First 3 requeriments implmented using tdd

--- src/main/java/com/chp03tictactoe/TicTacToe.java
+++ src/main/java/com/chp03tictactoe/TicTacToe.java
@@ -2,10 +2,38 @@ package com.chp03tictactoe;
 
 public class TicTacToe {
 
+    private static final String X_PIECE = "X";
+    private static final String O_PIECE = "O";
+
+    private String[][] board = new String[3][3];
 
     public void placePiece(String piece, int xAxis, int yAxis) throws RuntimeException{
+        validateXAxisPosition(xAxis);
+        validateYAxisPosition(yAxis);
+        validateIfPositionIsOcupated(xAxis, yAxis);
+        board[xAxis][yAxis] = piece;
+    }
+
+    private void validateXAxisPosition(int xAxis) throws RuntimeException{
         if(xAxis < 0 || xAxis > 3){
             throw new RuntimeException("X-Axis out of range, should be between 1-3");
+        }
+    }
+
+    private void validateYAxisPosition(int yAxis) throws RuntimeException{
+        if(yAxis < 0 || yAxis > 3){
+            throw new RuntimeException("Y-Axis out of range, should be between 1-3");
+        }
+    }
+
+    private void validateIfPositionIsOcupated(int xAxis, int yAxis) throws RuntimeException{
+        String pieceAtPosition = board[xAxis][yAxis];
+        if (pieceAtPosition != null && (X_PIECE.equals(pieceAtPosition) || pieceAtPosition.equals(O_PIECE))) {
+            StringBuilder builder = new StringBuilder();
+            builder
+                    .append("Position ").append(xAxis).append(" - ")
+                    .append(yAxis).append(" is already ocupated.");
+            throw new RuntimeException(builder.toString());
         }
     }
 
--- src/test/java/com/chp03tictactoe/TicTacToeSpec.java
+++ src/test/java/com/chp03tictactoe/TicTacToeSpec.java
@@ -10,7 +10,7 @@ public class TicTacToeSpec {
     static TicTacToe game;
 
     @Rule
-    public ExpectedException runtime = ExpectedException.none();
+    public ExpectedException exception = ExpectedException.none();
 
     @BeforeClass
     public static void setup(){
@@ -18,15 +18,42 @@ public class TicTacToeSpec {
     }
 
     @Test
-    public void xAxisTooLargeShouldThrowRuntimeException(){
+    public void xAxisTooOutOfRangeThrowsRuntimeException(){
 
         int x = 4;
         int y = 2;
         String piece = "X";
 
-        runtime.expect(RuntimeException.class);
+        exception.expect(RuntimeException.class);
 
         game.placePiece(piece, x, y);
 
     }
+
+    @Test
+    public void yAxisOutOfRangeThrowsRuntimeException(){
+        int x = 2;
+        int y = 4;
+        String piece = "O";
+
+        exception.expect(RuntimeException.class);
+
+        game.placePiece(piece, x, y);
+    }
+
+    @Test
+    public void placeAPieceInOcupatedPositionThrowsRuntimeException(){
+        int x = 2;
+        int y= 2;
+        String piece1 = "X";
+
+        game.placePiece(piece1, x, y);
+
+        String piece2 = "O";
+
+        exception.expect(RuntimeException.class);
+        game.placePiece(piece2, x, y);
+    }
+
+
 }
