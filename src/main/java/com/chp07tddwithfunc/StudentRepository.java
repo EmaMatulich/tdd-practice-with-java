4950c3e2676e403650768be86e814b67628610c3
ematulich <emanuel.matulich@mercadolibre.com>
2020-08-17 22:57:34 -0300

    Test for Student repository with optional being used

---
+++ src/main/java/com/chp07tddwithfunc/StudentRepository.java
@@ -0,0 +1,25 @@
+package com.chp07tddwithfunc;
+
+import java.util.*;
+
+public class StudentRepository {
+
+    private final Set<Student> students;
+
+    public StudentRepository(Collection<Student> students){
+        this.students = new HashSet<>(students);
+    }
+
+    public Optional<Student> findByName(String name){
+        return students.stream().filter(s -> s.name.equals(name)).findFirst();
+
+        //alternative
+/*        for (Student student: students){
+            if (student.name.equals(name)){
+                return Optional.of(student);
+            }
+        }
+        return Optional.empty();*/
+    }
+
+}
